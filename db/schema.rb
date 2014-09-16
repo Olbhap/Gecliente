f30cd615ce0adf30921b80d9c24d57e29041a8a9
Pabylonia <pablo@fraktal-design.net>
2014-09-16 12:14:19 +0200

    Añadido user_id a facturas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140907192431) do
+ActiveRecord::Schema.define(version: 20140916100955) do
 
   create_table "facturas", force: true do |t|
     t.date     "fecha_alta"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20140907192431) do
     t.text     "comentario"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "facturas", ["user_id"], name: "index_facturas_on_user_id"
 
   create_table "usuarios", force: true do |t|
     t.string   "nif",                                                 null: false
